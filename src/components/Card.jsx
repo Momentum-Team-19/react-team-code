6c871bd90e9b16586ab67905a53439157b926a16
davis <davisfpatterson@gmail.com>
2023-09-14 15:37:12 -0400

    added some logic for curCard and prevCard

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Card = ({ color }) => {
-    const [isFlipped, setIsFlipped] = useState(false)
-    const handleClick = () => {
-        setIsFlipped(!isFlipped)
-    }
-    return (
-        <React.Fragment>
-            <div className='card-container' onClick={handleClick}>
-                <>
-                    {isFlipped ? <p>{color}</p> : null}
-                </>
-            </div>
-        </React.Fragment>
-    );
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [curCard, setCurCard] = useState('');
+  const [prevCard, setPrevCard] = useState('');
+
+  const handleClick = () => {
+    let temp = curCard;
+    setIsFlipped(!isFlipped);
+    setCurCard(color);
+    setPrevCard(temp);
+    console.log('curCard: ', curCard);
+    console.log('prevCard: ', prevCard);
+  };
+
+  return (
+    <React.Fragment>
+      <div className='card-container' onClick={handleClick}>
+        <>{isFlipped ? <p>{color}</p> : null}</>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Card;
